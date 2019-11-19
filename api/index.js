4cf68205a980c50fea60f226c30ee94d5a8bc48d
marees <mareeswaran.sooriamoorthi@hidglobal.com>
2019-11-19 18:36:47 +0530

    Implemented hapi methods

--- api/index.js
+++ api/index.js
@@ -1,25 +1,52 @@
 module.exports = {
     name: "ApiPlugin",
     register: async (server, options) => {
+
+        server.method('add', (a, b) => {
+            return new Promise((resolve, reject) => {
+              setTimeout(() => {
+                resolve(a + b);
+              }, 300);
+            });
+        }, {
+            cache: {
+              expiresIn: 60000,
+              generateTimeout: 1000
+            }
+        });
+        
     
-      server.route([
-        {
-            method: "GET",
-            path: "/api/hello",
-            handler: async (request, h) => {
-                return "Hello, World";
+        server.route([
+            {
+                method: "GET",
+                path: "/api/hello",
+                handler: async (request, h) => {
+                    return "Hello, World";
+                }
+            },
+            {
+                method: "GET",
+                path: "/api/hello/html",
+                handler: async (request, h) => {
+                    const response = h.response('<html><head><title>hi</title></head><body><h1>Hello</h1></body></html>');
+                    response.type("text/html");
+                    return response;             
+                }
+            },
+            {
+                method: 'GET',
+                path: '/add',
+                handler: async (request, h) => {
+                    try {
+                        const { a, b } = request.query;
+                        const result = await server.methods.add(parseInt(a), parseInt(b));
+                        return result;
+                    } catch (err) {
+                        throw err;
+                    }
+                }
             }
-        },
-        {
-            method: "GET",
-            path: "/api/hello/html",
-            handler: async (request, h) => {
-                const response = h.response('<html><head><title>hi</title></head><body><h1>Hello</h1></body></html>');
-                response.type("text/html");
-                return response;             
-            }
-        }
-      ]);
+        ]);
     
     }
   }
