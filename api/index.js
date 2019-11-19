5891dc246377a7ab512a22e18b58ad2668338acf
marees <mareeswaran.sooriamoorthi@hidglobal.com>
2019-11-19 18:13:04 +0530

    Added nodemon setup and one more api

--- api/index.js
+++ api/index.js
@@ -4,12 +4,20 @@ module.exports = {
     
       server.route([
         {
-          method: "GET",
-          path: "/api/hello",
-          handler: async (request, h) => {
-            return "Hello, World";
-            
-          }
+            method: "GET",
+            path: "/api/hello",
+            handler: async (request, h) => {
+                return "Hello, World";
+            }
+        },
+        {
+            method: "GET",
+            path: "/api/hello/html",
+            handler: async (request, h) => {
+                const response = h.response('<html><head><title>hi</title></head><body><h1>Hello</h1></body></html>');
+                response.type("text/html");
+                return response;             
+            }
         }
       ]);
     
